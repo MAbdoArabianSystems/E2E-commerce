48ab1fbfde1a3c7cb9d844bb6a308cb060b65048
Front_Mohamed_Abdo <mohamed.abdo@arabiansystems.net>
2024-11-29 17:52:05 +0200

    create layouts common

---
+++ e2e-app/src/layouts/common/footer/index.jsx
@@ -0,0 +1,16 @@
+'use client'
+
+import MainFooter from './MainFooter';
+import Footer from './Footer';
+
+
+const Index = () => {
+  return (
+    <footer className='m-auto w-full bg-black/5 py-5'>
+      <MainFooter />
+      <Footer />
+    </footer>
+  )
+}
+
+export default Index
